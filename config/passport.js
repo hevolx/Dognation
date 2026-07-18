a54b0bcb185a19a0e88fb891aba65086cc8a753b
Love Holmgren <143357495+hevolx@users.noreply.github.com>
2026-07-18 19:26:26 +0200

    fix(passport): use helper.findByUsername in verify callback
    - replace undefined User.findOne call with helper.findByUsername
    - pass the plain username string instead of an object
    - add test verifying the local strategy calls helper.findByUsername
      with the username and a callback

--- config/passport.js
+++ config/passport.js
@@ -6,7 +6,12 @@ const helper = require("../helpers/helper");
 // Set up the Passport strategy:
 passport.use(new LocalStrategy(
   function (username, password, done) {
-
+    helper.findByUsername(username, function (err, user) {
+      if (err) return done(err);
+      if (!user) return done(null, false);
+      if (user.password != password) return done(null, false);
+      return done(null, user);
+    });
   }
 ));
 // Serialize a user
--- test/passport.test.js
+++ test/passport.test.js
@@ -1,8 +1,25 @@
+jest.mock("../helpers/helper", () => ({
+  findByUsername: jest.fn(),
+}));
+
 const passport = require("passport");
+const helper = require("../helpers/helper");
 require("../config/passport");
 
 describe("passport local strategy", () => {
   it("registers a local strategy with passport", () => {
     expect(passport._strategies.local).toBeDefined();
+  });
+
+  it("looks up the user via helper.findByUsername with the given username", () => {
+    const strategy = passport._strategies.local;
+    const done = jest.fn();
+
+    strategy._verify("myuser", "mypassword", done);
+
+    expect(helper.findByUsername).toHaveBeenCalledWith(
+      "myuser",
+      expect.any(Function)
+    );
   });
 });
